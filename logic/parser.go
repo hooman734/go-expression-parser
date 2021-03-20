5a7c88a70ed0e15b15cb9c25416771e1ec44610c
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2021-03-19 23:29:39 -0500

    Update parser.go

--- logic/parser.go
+++ logic/parser.go
@@ -62,51 +62,42 @@ func Evaluator(n Node) (float64, error) {
 		return atomicNode.Value, nil
 	case AddNode:
 		addNode := n.(AddNode)
-		leftR, errL := Evaluator(addNode.Left)
-		rightR, errR := Evaluator(addNode.Right)
-		if errL != nil || errR != nil {
-			err := fmt.Errorf("failed to add node %s %s %s", n, errL, errR)
-			return 0, err
-		} else {
-			return leftR + rightR, nil
-		}
+		return infixEvaluator(addNode.Left, addNode.Right, func(vl float64, vr float64) float64 {
+			return vl + vr
+		})
 	case SubtractNode:
 		subtractNode := n.(SubtractNode)
-		leftR, errL := Evaluator(subtractNode.Left)
-		rightR, errR := Evaluator(subtractNode.Right)
-		if errL != nil || errR != nil {
-			err := fmt.Errorf("failed to subtract node %s %s %s", n, errL, errR)
-			return 0, err
-		} else {
-			return leftR - rightR, nil
-		}
+		return infixEvaluator(subtractNode.Left, subtractNode.Right, func(vl float64, vr float64) float64 {
+			return vl - vr
+		})
 	case MultiplyNode:
 		multiplyNode := n.(MultiplyNode)
-		leftR, errL := Evaluator(multiplyNode.Left)
-		rightR, errR := Evaluator(multiplyNode.Right)
-		if errL != nil || errR != nil {
-			err := fmt.Errorf("failed to multiply node %s %s %s", n, errL, errR)
-			return 0, err
-		} else {
-			return leftR * rightR, nil
-		}
+		return infixEvaluator(multiplyNode.Left, multiplyNode.Right, func(vl float64, vr float64) float64 {
+			return vl * vr
+		})
 	case DivideNode:
 		divideNode := n.(DivideNode)
-		leftR, errL := Evaluator(divideNode.Left)
-		rightR, errR := Evaluator(divideNode.Right)
-		if errL != nil || errR != nil {
-			err := fmt.Errorf("failed to divide node %s %s %s", n, errL, errR)
-			return 0, err
-		} else {
-			return leftR / rightR, nil
-		}
+		return infixEvaluator(divideNode.Left, divideNode.Right, func(vl float64, vr float64) float64 {
+			return vl / vr
+		})
 	default:
 		err := fmt.Errorf("failed to evaluate node %s", n)
 		return 0, err
 	}
 }
 
-func combinations(str string, sep string) [][2]string {
+func infixEvaluator(leftN Node, rightN Node, op func(vl float64, vr float64) float64) (float64, error)  {
+	leftR, errL := Evaluator(leftN)
+	rightR, errR := Evaluator(rightN)
+	if errL != nil || errR != nil {
+		err := fmt.Errorf("failed to evaluate infix nodes %s %s", errL, errR)
+		return 0, err
+	} else {
+		return op(leftR, rightR), nil
+	}
+}
+
+func generateCombinations(str string, sep string) [][2]string {
 	result := strings.Split(str, sep)
 	combinations := make([][2]string, len(result)-1)
 
@@ -117,7 +108,7 @@ func combinations(str string, sep string) [][2]string {
 	return combinations
 }
 
-func infix(tokens [2]string, operator string) (Node, Node, error) {
+func infixParser(tokens [2]string, operator string) (Node, Node, error) {
 	leftN, errL := Parser(tokens[0])
 	rightN, errR := Parser(tokens[1])
 
@@ -131,6 +122,7 @@ func infix(tokens [2]string, operator string) (Node, Node, error) {
 
 func Parser(str string) (Node, error) {
 	operators := [5]int{'+', '-', '*', '/'}
+
 	str = strings.TrimSpace(str)
 
 	if str == "" {
@@ -141,8 +133,8 @@ func Parser(str string) (Node, error) {
 	for i := 0; i < len(operators); i++ {
 		operator := string(rune(operators[i]))
 		if strings.Contains(str, operator) {
-			for _, result := range combinations(str, operator) {
-				leftN, rightN, err := infix(result, operator)
+			for _, result := range generateCombinations(str, operator) {
+				leftN, rightN, err := infixParser(result, operator)
 
 				if err != nil {
 					continue
