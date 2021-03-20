5919bd2d005d5ef70a49fd105f816a9ecef08678
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2021-03-19 23:18:26 -0500

    Update app.go

--- app.go
+++ app.go
@@ -6,7 +6,14 @@ import (
 )
 
 func main() {
-	n, _ := Parser(" 2 * +3  - 6 / 4 + 5")
-	v, _ := Evaluator(n)
+	n, parseError := Parser("2 * +3 - 6 / 4 + 5")
+	if parseError != nil {
+		panic(parseError)
+	}
+
+	v, evalError := Evaluator(n)
+	if evalError != nil {
+		panic(evalError)
+	}
 	fmt.Printf("Result: %s = %f", n.ToString(), v)
 }
